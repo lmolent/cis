2a4bd7b0ac9252776a5b6276e869b97c61eba268
Lubo Molent <you@example.com>
2022-02-09 15:01:39 +0100

    scripts

--- mktar.sh
+++ mktar.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
+RAW=$1
 MNTDIR=$(mktemp -d)
 
-sudo losetup -P /dev/loop99 desktop.raw 
+sudo losetup -P /dev/loop99 $RAW
 
 sudo mount /dev/loop99p3 $MNTDIR
 sudo mount /dev/loop99p2 $MNTDIR/boot/efi
 
 sudo tar -Szcpf desktop.tgz --acls --selinux --xattrs -C ./scripts . -C $MNTDIR .
-
-#sudo tar -rvf desktop.tar curtin
-
-#sudo gzip desktop.tar
 
 sudo umount $MNTDIR/boot/efi
 sudo umount $MNTDIR
